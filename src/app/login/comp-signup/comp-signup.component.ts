bcd050a40e9695bb1b86d32991b2e3722b6528af
Sunil Kumar T <s1728k@gmail.com>
2019-03-04 19:42:08 +0530

    a1

--- src/app/login/comp-signup/comp-signup.component.ts
+++ src/app/login/comp-signup/comp-signup.component.ts
@@ -18,7 +18,7 @@ export class CompSignupComponent implements OnInit {
   showReferralCode = false;
   showAltReferralCode = false
   r6 = ['','','','','',''];
-  error = {};
+  e = {};
 
   constructor(private http: RestApiService, private appStore: AppStore, private router: Router,) { }
 
@@ -55,69 +55,19 @@ export class CompSignupComponent implements OnInit {
   }
 
   saveCompany(){
-    this.error = {};
-    this.validateNewCompanyEmail();
-    this.validateNewCompanyPassword();
-    this.validateNewCompany();
-    console.log(this.error)
-    if (Object.getOwnPropertyNames(this.error).length === 0){
       this.http.url = environment.baseUrl + '1';
       this.company['table'] = 'company';
       this.company['secret'] = "$2y$10$IKTMx1reCFHIgOTzqQoPeukNferN5Z10bFol.itjyclfX7BAxtf4m";
-      this.http.addObj(this.company).subscribe((data:any)=>{
-        console.log(data);
-        if(data){
-          // this.router.navigate(['/login']);
+      this.http.addObj(this.company).subscribe((res)=>{
+        console.log(res);
+        if(res['status']=='success'){
+          if(res['data']['errors']){
+            this.e = res['data']['errors'];
+          }
         }else{
-          console.log(this.appStore.errors['error']['errors']);
-          this.error = this.appStore.errors['error']['errors'];
+          console.log(res);
         }
       });
-    }
-  }
-
-  assert(field, value){
-    if(!value){this.error[field]="visible";this.error[field + '_ev']="Mandatory Field. ";}
-  }
-
-  validateNewCompany(){
-    this.assert('company_name', this.company['company_name'])
-    this.assert('full_name', this.company['full_name'])
-    this.assert('tel', this.company['tel'])
-    this.assert('skype', this.company['skype'])
-    this.assert('email', this.company['email'])
-    this.assert('password', this.company['password'])
-  }
-
-  validateNewCompanyEmail(){
-    if(!this.validateEmail(this.company['email'])){
-      this.error['email_ev'] = "Invalid Email ID";
-      this.error['email']="visible";
-    }
-  }
-
-  validateNewCompanyPassword(){
-    if(!this.validatePassword(this.company['password'])){
-      this.error['password_ev'] = "Password should contain atleat one number and one special charector";
-      this.error['password']="visible";
-    }
-  }
-
-  validateNewCompanyConfirmPassword(){
-    if(this.company['password'] != this.company['password_confirmation']){
-      this.error['password_confirmation_ev'] = "Passwords did not match";
-      this.error['password_confirmation']="visible";
-    }
-  }
-
-  validateEmail(email) {
-      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-      return re.test(email);
-  }
-
-  validatePassword(password) {
-      var re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/i;
-      return re.test(password);
   }
 
 }
--- src/app/login/dev-signup/dev-signup.component.ts
+++ src/app/login/dev-signup/dev-signup.component.ts
@@ -14,7 +14,7 @@ export class DevSignupComponent implements OnInit {
 
   dev = new Dev();
   pwShow = false;
-  error = {};
+  e = {};
 
   constructor(private http: RestApiService, private appStore: AppStore, private router: Router,) { }
 
@@ -26,65 +26,20 @@ export class DevSignupComponent implements OnInit {
   }
 
   saveDev(){
-    this.error = {};
-    this.validateNewDevEmail();
-    this.validateNewDevPassword();
-    this.validateNewDevConfirmPassword();
-    this.validateNewDev();
-    if (Object.getOwnPropertyNames(this.error).length === 0){
       this.http.url = environment.baseUrl + '1';
       this.dev['table'] = this.dev['profession'];
       this.dev['secret'] = "$2y$10$IKTMx1reCFHIgOTzqQoPeukNferN5Z10bFol.itjyclfX7BAxtf4m";
-      this.http.addObj(this.dev).subscribe((data:any)=>{
-        console.log(data);
+      this.http.addObj(this.dev).subscribe((res:any)=>{
+        console.log(res);
+        if(res['status']=='success'){
+          if(res['data']['errors']){
+            this.e = res['data']['errors'];
+            console.log(this.e);
+          }
+        }else{
+          console.log(res);
+        }
       });
-      // this.router.navigate(['/login']);
-    }
-  }
-
-  assert(field, value){
-    if(!value){this.error[field]="visible";this.error[field + '_ev']="Mandatory Field. ";}
-  }
-
-  validateNewDev(){
-    this.assert('full_name', this.dev['full_name']);
-    this.assert('tel', this.dev['tel']);
-    this.assert('skype', this.dev['skype']);
-    this.assert('profession', this.dev['profession']);
-    this.assert('email', this.dev['email']);
-    this.assert('password', this.dev['password']);
-    this.assert('password_confirmation', this.dev['password_confirmation']);
-  }
-
-  validateNewDevEmail(){
-    if(!this.validateEmail(this.dev['email'])){
-      this.error['email_ev'] = "Invalid Email ID";
-      this.error['email']="visible";
-    }
-  }
-
-  validateNewDevPassword(){
-    if(!this.validatePassword(this.dev['password'])){
-      this.error['password_ev'] = "Password should contain atleat one number and one special charector";
-      this.error['password']="visible";
-    }
-  }
-
-  validateNewDevConfirmPassword(){
-    if(this.dev['password'] != this.dev['password_confirmation']){
-      this.error['password_confirmation_ev'] = "Passwords did not match";
-      this.error['password_confirmation']="visible";
-    }
-  }
-
-  validateEmail(email) {
-      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-      return re.test(email);
-  }
-
-  validatePassword(password) {
-      var re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/i;
-      return re.test(password);
   }
 
 }
